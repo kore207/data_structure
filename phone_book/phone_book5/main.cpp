77bb4944a367a3df19681ccc3c86ac513e681cbc
DESKTOP-9L4KOLL\K <kore207@naver.com>
2018-12-27 22:53:50 +0900

    자료구조 전화번호부 예제 ver.5
     call-by-value or pass-by-assignment
    값에의한 호출 ->호출시에 모든 멤버들이 복사됨(C언어 매개변수 전달의 기본 방식)
    구조체에서는 복사되는 양이 많아 진다.
    특히 return 문에서는 내부적으로 두번 복사 되는 과정이 있다.
    ->구조체의 배열을 사용하지 않고 구조체의 포인터타입의 배열을 사용한다. -> 단지 주소값이 복사됨
    :(*p).name  => p->name

---
+++ phone_book/phone_book5/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ //call-by-value or pass-by-assignment
+//값에의한 호출 ->호출시에 모든 멤버들이 복사됨(C언어 매개변수 전달의 기본 방식)
+//구조체에서는 복사되는 양이 많아 진다.
+//특히 return 문에서는 내부적으로 두번 복사 되는 과정이 있다. 
+
+int main() {
+
+}

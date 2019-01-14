54074be60070a559d33fbc7ee6d4c5e95b6911e4
gt <kore17641764@gmail.com>
2019-01-14 21:09:49 +0900

    Data Struct Ex- Music Library Program

---
+++ Music Library Program/Music Library Program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Music Library Program
+//
+//  Created by Kim GT  on 14/01/2019.
+//  Copyright © 2019 Kim GT . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
